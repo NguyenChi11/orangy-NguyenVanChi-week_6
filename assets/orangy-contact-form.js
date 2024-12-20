7224a7ad0b4cde2f2b0107280b5703e37c5cb64b
Chí-k62 Nguyễn <chinguyen280200@gmail.com>
2024-12-20 15:07:49 +0700

    orangy-edit-contact: uapdate popup contact

--- assets/orangy-contact-form.js
+++ assets/orangy-contact-form.js
@@ -1,91 +1,93 @@
-const formEl = document.querySelector(".form__wrapper");
-const fullName = formEl.querySelector("#fullname");
-const email = formEl.querySelector("#email");
-const phone = formEl.querySelector("#phone");
-const address = formEl.querySelector("#address");
-const message = document.querySelector("#text-message");
+document.addEventListener("DOMContentLoaded", function () {
+  const formEl = document.querySelector(".form__wrapper");
+  const fullName = formEl.querySelector("#fullname");
+  const email = formEl.querySelector("#email");
+  const phone = formEl.querySelector("#phone");
+  const address = formEl.querySelector("#address");
+  const message = document.querySelector("#text-message");
 
-const popupSuccess = document.querySelector(".popup-success");
-const popupError = document.querySelector(".popup-error");
-const turnOff = document.querySelector(".icon-popup-fetch");
+  const popupSuccess = document.querySelector(".popup-success");
+  const popupError = document.querySelector(".popup-error");
+  const turnOff = document.querySelector(".icon-popup-fetch");
 
-const recaptcha = document.querySelector(".popup-recaptcha");
-const btnRecaptcha = document.querySelector(".btn-recaptcha");
+  const recaptcha = document.querySelector(".popup-recaptcha");
+  const btnRecaptcha = document.querySelector(".btn-recaptcha");
 
-const inputs = document.querySelectorAll(".input-form");
-// console.log(inputs);
-// console.log(fullName);
-// console.log(email);
-// console.log(phone);
-// console.log(address);
-// console.log(message);
+  const inputs = document.querySelectorAll(".input-form");
+  // console.log(inputs);
+  // console.log(fullName);
+  // console.log(email);
+  // console.log(phone);
+  // console.log(address);
+  // console.log(message);
 
-formEl.addEventListener("submit", (event) => {
-  event.preventDefault();
-  // Kiểm tra nếu trường nào bị thiếu dữ liệu
-  let isValidInput = true;
+  formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Kiểm tra nếu trường nào bị thiếu dữ liệu
+    let isValidInput = true;
 
-  // Kiểm tra từng input
-  inputs.forEach((input) => {
-    if (input.value.trim() === "") {
-      isValidInput = false;
-      input.focus();
-      return;
+    // Kiểm tra từng input
+    inputs.forEach((input) => {
+      if (input.value.trim() === "") {
+        isValidInput = false;
+        input.focus();
+        return;
+      }
+    });
+
+    if (isValidInput) {
+      let body = JSON.stringify({
+        fullname: fullName.value.trim(),
+        email: email.value.trim(),
+        phone: phone.value.trim(),
+        address: address.value.trim(),
+        message: message.value.trim(),
+      });
+
+      // Gửi dữ liệu qua fetch
+      fetch("https://testapi.demo.wgentech.com/notify.php", {
+        body,
+        method: "POST",
+        keepalive: true,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // Log response từ server
+          if (!data.success) {
+            console.log(body);
+            // Lưu thông tin vào Local Storage
+            localStorage.setItem("userData", body);
+          }
+          recaptcha.classList.add("action");
+          btnRecaptcha.addEventListener("click", () => {
+            recaptcha.classList.remove("action");
+          });
+          showPopupSuccess();
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          popupError.classList.add("action");
+          turnOff.addEventListener("click", () => {
+            popupError.classList.remove("action");
+          });
+        });
+      function showPopupSuccess() {
+        popupSuccess.classList.add("action");
+        turnOff.addEventListener("click", () => {
+          popupSuccess.classList.remove("action");
+        });
+      }
     }
   });
 
-  if (isValidInput) {
-    let body = JSON.stringify({
-      fullname: fullName.value.trim(),
-      email: email.value.trim(),
-      phone: phone.value.trim(),
-      address: address.value.trim(),
-      message: message.value.trim(),
-    });
-
-    // Gửi dữ liệu qua fetch
-    fetch("https://testapi.demo.wgentech.com/notify.php", {
-      body,
-      method: "POST",
-      keepalive: true,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data); // Log response từ server
-        if (!data.success) {
-          console.log(body);
-          // Lưu thông tin vào Local Storage
-          localStorage.setItem("userData", body);
-        }
-        recaptcha.classList.add("action");
-        btnRecaptcha.addEventListener("click", () => {
-          recaptcha.classList.remove("action");
-        });
-        showPopupSuccess();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        popupError.classList.add("action");
-        turnOff.addEventListener("click", () => {
-          popupError.classList.remove("action");
-        });
-      });
-    function showPopupSuccess() {
-      popupSuccess.classList.add("action");
-      turnOff.addEventListener("click", () => {
-        popupSuccess.classList.remove("action");
-      });
+  window.addEventListener("load", () => {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData) {
+      fullName.value = userData.fullname;
+      phone.value = userData.phone;
+      email.value = userData.email;
+      address.value = userData.address;
+      message.value = userData.message;
     }
-  }
-});
-
-window.addEventListener("load", () => {
-  const userData = JSON.parse(localStorage.getItem("userData"));
-  if (userData) {
-    fullName.value = userData.fullname;
-    phone.value = userData.phone;
-    email.value = userData.email;
-    address.value = userData.address;
-    message.value = userData.message;
-  }
+  });
 });
--- assets/orangy-newsletter.js
+++ assets/orangy-newsletter.js
@@ -1,55 +1,57 @@
-const submitFooter = document.querySelector("#submit-footer");
-const inputFooter = document.querySelector("#input-footer");
-const formFooter = document.querySelector(".footer__input");
+document.addEventListener("DOMContentLoaded", function () {
+  const submitFooter = document.querySelector("#submit-footer");
+  const inputFooter = document.querySelector("#input-footer");
+  const formFooter = document.querySelector(".footer-block__newsletter");
 
-const popupSuccess = document.querySelector(".popup-success");
-const popupError = document.querySelector(".popup-error");
-const turnOff = document.querySelector(".icon-popup-fetch");
-const turnError = document.querySelector(".icon-popup-error");
+  const popupSuccess = document.querySelector(".popup-success");
+  const popupError = document.querySelector(".popup-error");
+  const turnOff = document.querySelector(".icon-popup-fetch");
+  const turnError = document.querySelector(".icon-popup-error");
 
-// Hàm kiểm tra email hợp lệ
-function isValidEmail(email) {
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(email);
-}
-
-formFooter.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const emailValue = inputFooter.value;
-  // Kiểm tra email hợp lệ
-  if (!isValidEmail(emailValue)) {
-    popupError.classList.add("action");
-    turnError.addEventListener("click", () => {
-      popupError.classList.remove("action");
-    }); // Hoặc hiển thị thông báo bằng popup tùy bạn
-    return;
+  // Hàm kiểm tra email hợp lệ
+  function isValidEmail(email) {
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(email);
   }
 
-  // Tạo FormData nếu email hợp lệ
-  const formData = new FormData();
-  formData.append("email", emailValue);
-
-  fetch(
-    "https://script.google.com/macros/s/AKfycbwlnruolAEFek8x1wUYLxocHdIuwHWrHYXSfzSXZSygslzejM-ppDe76EuK8jkKWyB4/exec",
-    {
-      method: "POST",
-      body: formData,
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      popupSuccess.classList.add("action");
-      turnOff.addEventListener("click", () => {
-        popupSuccess.classList.remove("action");
-      });
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+  formFooter.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const emailValue = inputFooter.value;
+    // Kiểm tra email hợp lệ
+    if (!isValidEmail(emailValue)) {
       popupError.classList.add("action");
-      turnOff.addEventListener("click", () => {
+      turnError.addEventListener("click", () => {
         popupError.classList.remove("action");
+      }); // Hoặc hiển thị thông báo bằng popup tùy bạn
+      return;
+    }
+
+    // Tạo FormData nếu email hợp lệ
+    const formData = new FormData();
+    formData.append("email", emailValue);
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbwlnruolAEFek8x1wUYLxocHdIuwHWrHYXSfzSXZSygslzejM-ppDe76EuK8jkKWyB4/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        popupSuccess.classList.add("action");
+        turnOff.addEventListener("click", () => {
+          popupSuccess.classList.remove("action");
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        popupError.classList.add("action");
+        turnOff.addEventListener("click", () => {
+          popupError.classList.remove("action");
+        });
       });
-    });
+  });
 });
